916d90140e824cc719124f4b51368a9b6d71598d
Loïc Petit <loic.petit@data-publica.com>
2016-04-29 19:50:04 +0200

    Vocab testing

---
+++ vocab/app.js
@@ -0,0 +1,61 @@
+var content = null;
+var index = -10;
+var tries = 0;
+var start = null;
+
+var templateResult = '<tr><td><span class="glyphicon glyphicon-$1" aria-hidden="true"></span></td><td>$2</td><td>$3</td><td>$4</td><td>$5</td></tr>';
+
+function draw() {
+    if (content == null || index < 0) {
+        return;
+    }
+    var isKata = $("#type").val() == "kata";
+    var txt = content[index];
+    $("#label").text(isKata ? wanakana.toKatakana(wanakana.toRomaji(txt)): txt);
+    $("#test").focus();
+}
+
+function next() {
+    index = Math.floor(Math.random()*content.length);
+    start = new Date().getTime();
+    tries = 0;
+    $("#test").val("");
+    draw();
+}
+
+function getTypingSpeed(toGuess) {
+    var speed = (((new Date().getTime() - start) / 1000) / toGuess.length);
+    return speed.toFixed(2);
+}
+function getTemplateResult(result, given, expected) {
+    var toGuess = $("#label").text();
+    return $(templateResult
+        .replace('$1', result ? "ok" : "remove")
+        .replace('$2', toGuess)
+        .replace('$3', given)
+        .replace('$4', expected)
+        .replace('$5', getTypingSpeed(toGuess)+" sec/char")
+    );
+}
+
+function answer(result) {
+    var romanji = wanakana.toRomaji(content[index]);
+    if (result.trim() == romanji) {
+        $("#result tbody").prepend(getTemplateResult(true, result, ""));
+        next();
+    } else {
+        tries += 1;
+        if (tries == parseInt($("#max-tries").val())) {
+            $("#result tbody").prepend(getTemplateResult(false, result, romanji));
+            next();
+        } else {
+            $("#result tbody").prepend(getTemplateResult(false, result, ""));
+            start = new Date().getTime();
+        }
+    }
+}
+
+/*$.ajax("hira.list").done(function(data) {
+    content = data.split("\n");
+    next();
+});*/
