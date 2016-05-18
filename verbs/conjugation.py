432a643fdc861c5dd8df6dcaaf2ae383c6a9f269
WydD <loic.petit@gmail.com>
2016-05-18 03:57:18 +0200

    More or less working conjugation for plain/masu/negative

--- verbs/conjugation.py
+++ verbs/conjugation.py
@@ -5,9 +5,10 @@ import romkan
 class Plain:
     def __init__(self, verb):
         self.verb = verb
+        self.is_ru = verb.is_ru
 
     def present(self):
-        return self.verb.base(BaseForms.PLAIN)
+        return self.verb.base(BaseForms.U)
 
     def past(self):
         # TE form with an a
@@ -15,17 +16,122 @@ class Plain:
         hepburn[-1] = 'a'
         return romkan.to_hiragana(hepburn)
 
-    def teiru(self):
-        return self.verb.base(BaseForms.TE)+"いる"
-
     def te(self):
         return self.verb.base(BaseForms.TE)
 
-    def tai(self):
-        return self.verb.base(BaseForms.MASU)+"たい"
+    def i(self):
+        return self.verb.base(BaseForms.I)
 
     def passive(self):
-        return self.verb.base(BaseForms.NEGATIVE) + "れる"
+        return self.verb.base(BaseForms.A) + "れる"
 
     def causative(self):
-        return self.verb.base()
+        if self.is_ru:
+            return self.verb.stem() + "させる"
+        return self.verb.base(BaseForms.A) + "せる"
+
+    def causative_passive(self):
+        # Short for interpreting the causative form as a RU verb
+        # Equivalent to Plain(RUVerb(self.causative())).passive()
+        return self.causative()[0:-1] + "られる"
+
+    def volitional(self):
+        return self.verb.base(BaseForms.O)
+
+    def imperative(self):
+        if self.is_ru:
+            return self.verb.stem() + "ら"
+        return self.verb.base(BaseForms.E)
+
+    def conditional(self):
+        return self.verb.base(BaseForms.E) + "ば"
+
+    def conditional_past(self):
+        return self.past() + "ら"
+
+    def potential(self):
+        if self.is_ru:
+            return self.verb.stem() + "られる"
+        return self.verb.base(BaseForms.E) + "る"
+
+
+class Polite(Plain):
+    def __init__(self, verb):
+        super().__init__(verb)
+        self.masu_base = self.i()
+
+    def present(self):
+        return self.masu_base + "ます"
+
+    def past(self):
+        return self.masu_base + "ました"
+
+    def volitional(self):
+        return self.masu_base + "ましょう"
+
+    def conditional(self):
+        return self.present() + "れば"  # alternative: ませば / is it ますなれば?
+
+    def conditional_past(self):
+        # Same as plain
+        return self.past() + "ら"
+
+    def imperative(self):
+        return self.te() + " ください"
+
+    def passive(self):
+        # Re conjugation as the present of the passive (ru form)
+        return Plain.passive(self)[0:-1] + "ます"
+
+    def causative(self):
+        # Re conjugation as the present of the causative (ru form)
+        return Plain.causative(self)[0:-1] + "ます"
+
+    def causative_passive(self):
+        # Re conjugation as the present of the causative (ru form)
+        return Plain.causative_passive(self)[0:-1] + "ます"
+
+    def potential(self):
+        # Re conjugation as the present of the potential (ru form)
+        return Plain.potential(self)[0:-1] + "ます"
+
+
+class Negative(Plain):
+    def __init__(self, verb):
+        super().__init__(verb)
+        self.negative_base = self.verb.base(BaseForms.A)
+
+    def present(self):
+        return self.negative_base + "ない"
+
+    def past(self):
+        return self.negative_base + "なかった"
+
+    def volitional(self):
+        return (self.verb.stem() if self.is_ru else self.verb.base(BaseForms.U)) + "まい"
+
+    def conditional(self):
+        return self.negative_base + "なければ"
+
+    def conditional_past(self):
+        # Same as plain
+        return self.past() + "ら"
+
+    def imperative(self):
+        return self.present() + "で"
+
+    def passive(self):
+        # Re conjugation as the present of the passive (ru form)
+        return Plain.passive(self)[0:-1] + "ない"
+
+    def causative(self):
+        # Re conjugation as the present of the causative (ru form)
+        return Plain.causative(self)[0:-1] + "ない"
+
+    def causative_passive(self):
+        # Re conjugation as the present of the causative (ru form)
+        return Plain.causative_passive(self)[0:-1] + "ない"
+
+    def potential(self):
+        # Re conjugation as the present of the potential (ru form)
+        return Plain.potential(self)[0:-1] + "ない"
--- verbs/verb.py
+++ verbs/verb.py
@@ -2,32 +2,37 @@ from enum import Enum
 
 
 class BaseForms(Enum):
-    NEGATIVE = "a"
-    CONDITIONAL = "e"
+    A = "a"
+    E = "e"
     TE = "te"
-    MASU = "i"
-    PLAIN = "u"
-    VOLITIONAL = "o"
+    I = "i"
+    U = "u"
+    O = "o"
     POTENTIAL = "rare"
     IMPERATIVE = "ro"
+    CAUSATIVE = "sa"
 
 
 class Verb:
     _base_table = None
+    is_ru = False
 
     def __init__(self, plain_form):
         self._plain_form = plain_form
         self._stem = plain_form[0:-1]
 
     def base(self, base_form):
-        if base_form == BaseForms.PLAIN:
+        if base_form == BaseForms.U:
             return self._plain_form
         return self._stem + self._base_table[base_form.value]
 
+    def stem(self):
+        return self._stem
 
 class RUVerb(Verb):
     def __init__(self, plain_form):
         super().__init__(plain_form)
+        self.is_ru = True
         self._base_table = {
             "a": "",
             "i": "",
@@ -36,4 +41,5 @@ class RUVerb(Verb):
             "o": "よう",
             "rare": "られ",
             "ro": "ろ",
+            "sa": "さ",
         }
